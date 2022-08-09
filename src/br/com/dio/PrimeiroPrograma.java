ed58dd8435925b6f7ea2e067aa1e97338b3af622
Paula <akei.umekawa@hotmail.com>
2022-08-09 19:43:43 -0300

    first commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,11 +1,23 @@
 package br.com.dio;
 
+import br.com.dio.model.Gato;
+
 public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
+		
 		Gato gato = new Gato();
-		Livro livro = new Livro();
+		Livros livros = new Livros();
+		
 		
 		System.out.println(gato);
-		System.out.println(livro);
-		
+		System.out.println(livros);
+	}
+	
+}
+	
+	class Livros {
+		private String nome;
+		private String npag;
+
+}
